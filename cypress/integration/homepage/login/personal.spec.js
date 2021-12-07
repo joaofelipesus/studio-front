ca804512dfa988eb11ef2c0e7c4e25a699b48256
joaofelipe <joaofelipe1294@gmail.com>
2021-12-07 17:02:49 -0300

    chore: cypress configs.

--- cypress/integration/homepage/login/personal.spec.js
+++ cypress/integration/homepage/login/personal.spec.js
@@ -18,6 +18,7 @@ describe('example to-do app', () => {
         "token": "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiZDVkMTcxODktZWM1Yy00NWJmLTk5YjQtOTc2MGIwZWQzYzVhIiwia2luZCI6InBlcnNvbmFsIiwiZXhwIjoxNjM4OTE5Njk4fQ.wRMIHOoTbNELyNeOkGXe2PB-6hddGQUss3fiEkJqoRA"
       }
     ).as('authUser')
+    cy.get('#nav-dropdown').click();
     cy.get('[routerlink="/login"]').click();
     cy.get('#email').type('asuka@nerv.com');
     cy.get('#password').type('123123123');
@@ -35,6 +36,7 @@ describe('example to-do app', () => {
         "token": "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiNTk3ODZkMTEtYmZjNS00OWFiLThiYmYtYjQwMGNkNTJjYTViIiwia2luZCI6InN0dWRlbnQiLCJleHAiOjE2Mzg5MjAwMDd9.925SD6TaFudT3ZjvDUcAy8hlaeRrW_X-5oKjW0XTGLk"
       }
     ).as('authUser')
+    cy.get('#nav-dropdown').click();
     cy.get('[routerlink="/login"]').click();
     cy.get('#email').type('shinji@nerv.com');
     cy.get('#password').type('123123123');
