4d765d708278d8b990db999ea663cbaf492225ba
joaofelipe <joaofelipe1294@gmail.com>
2021-12-07 09:33:44 -0300

    feat: add login form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './homepage/home/home.component';
 import { LoginComponent } from './homepage/login/login.component';
 import { HomeNavbarComponent } from './homepage/home-navbar/home-navbar.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { HomeNavbarComponent } from './homepage/home-navbar/home-navbar.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/homepage/login/login.component.ts
+++ src/app/homepage/login/login.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  email: string = "";
+  password: string = ''
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  login(){}
 }
