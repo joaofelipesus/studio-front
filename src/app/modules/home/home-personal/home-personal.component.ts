97f6be01a607b761c99f4fe09484be22ee863eb2
joaofelipe <joaofelipe1294@gmail.com>
2021-12-17 22:11:33 -0300

    feat: add home-personal component.

---
+++ src/app/modules/home/home-personal/home-personal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-personal',
+  templateUrl: './home-personal.component.html',
+  styleUrls: ['./home-personal.component.scss']
+})
+export class HomePersonalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -5,10 +5,10 @@ import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { ComponentsModule } from 'src/app/components/components.module';
-
+import { HomePersonalComponent } from './home-personal/home-personal.component';
 
 @NgModule({
-  declarations: [HomeComponent, LoginComponent],
+  declarations: [HomeComponent, LoginComponent, HomePersonalComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
