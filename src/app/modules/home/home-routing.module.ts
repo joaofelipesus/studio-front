e4f593cc5bcb1411beb074acfdcc82ce5ca223ca
joaofelipe <joaofelipe1294@gmail.com>
2021-12-17 23:16:55 -0300

    feat: add guard used to validate if user is a personal.

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -3,11 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomePersonalComponent } from './home-personal/home-personal.component';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
+import { PersonalAuthenticatedGuard } from 'src/app/guards/personal-authenticated.guard';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'personal', component: HomePersonalComponent}
+  {path: 'personal', component: HomePersonalComponent, canActivate: [PersonalAuthenticatedGuard]}
 ];
 
 @NgModule({
