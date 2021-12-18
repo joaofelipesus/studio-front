3fbe7a43a94d14dfe1e09c5b545aab5582a4d99f
joaofelipe <joaofelipe1294@gmail.com>
2021-12-17 22:20:37 -0300

    feat: implement logout.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  private SESSION_KEY = 'authToken';
 
-  ngOnInit(): void {
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {}
+
+  logedIn() : boolean {
+    const userSession = localStorage.getItem(this.SESSION_KEY);
+    if (userSession === null)
+      return false;
+
+    return true;
+  }
+
+  logout() : void {
+    localStorage.removeItem(this.SESSION_KEY);
+    this.router.navigateByUrl('');
   }
 
 }
