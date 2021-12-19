8082de7ed3c7e3e15f51f742727cff6a7003cb7e
Joao Felipe <joaofelipe1294@gmail.com>
2021-12-18 22:26:31 -0300

    test: add authenticate service coverage.

--- src/app/modules/home/services/authenticate.service.spec.ts
+++ src/app/modules/home/services/authenticate.service.spec.ts
@@ -1,16 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { AuthenticateService } from './authenticate.service';
 
 describe('AuthenticateService', () => {
   let service: AuthenticateService;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['post']);
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: HttpClient, useValue: httpClientSpy }
+      ]
+    });
     service = TestBed.inject(AuthenticateService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('#authenticate should return an observable', () => {
+    service.authenticate('asuka@nerv.com', '123123123')
+    expect(httpClientSpy.post.calls.count()).toBe(1);
+  })
 });
