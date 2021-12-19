114c5d4cb9edcb82e5459d89e4ca7d1dfaa8434a
Joao Felipe <joaofelipe1294@gmail.com>
2021-12-18 21:25:23 -0300

    test: remove login request mocks.

--- cypress/integration/homepage/login/personal.spec.js
+++ cypress/integration/homepage/login/personal.spec.js
@@ -2,21 +2,17 @@ import { personalDoLogin } from "../../../helpers/user_actions";
 
 describe('example to-do app', () => {
   beforeEach(() => {
+    cy.request('POST', 'localhost:3000/api/e2e/setup')
     cy.visit('http://localhost:4200/');
   });
 
   it('redirect to home page if try to access path without login', () => {
     cy.visit('http://localhost:4200/home/personal');
-    cy.url().should('equal', 'http://localhost:4200/home')
+    cy.url().should('equal', 'http://localhost:4200/home/login')
   })
 
   it('redirects to personal home page when login is correct', () => {
     personalDoLogin(cy)
     cy.url().should('equal', 'http://localhost:4200/home/personal');
   })
-
-  it('redirects to home page when user token belongs to a student', () => {
-    cy.url().should('equal', 'http://localhost:4200/home');
-  })
-
 })
