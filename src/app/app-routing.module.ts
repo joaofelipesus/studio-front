7bfde371a0a5ff4dee2b830c9012167c6ddf9bf3
joaofelipe <joaofelipe1294@gmail.com>
2021-12-20 17:01:11 -0300

    feat: add student module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', loadChildren: () => import('src/app/modules/home/home.module').then((m) => m.HomeModule) },
-  {path: 'exercises', loadChildren: () => import('src/app/modules/exercise/exercise.module').then((m) => m.ExerciseModule) }
+  {path: 'exercises', loadChildren: () => import('src/app/modules/exercise/exercise.module').then((m) => m.ExerciseModule) },
+  {path: 'students', loadChildren: () => import('src/app/modules/student/student.module').then((m) => m.StudentModule) }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,35 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { HomeComponent } from './views/home/home/home.component';
-// import { HomeNavbarComponent } from './views/home/home-navbar/home-navbar.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from  '@angular/common/http';
-// import { PersonalComponent } from './views/home/personal/personal.component';
-// import { PersonalNavbarComponent } from './views/home/personal-navbar/personal-navbar.component';
-// import { CreateExerciseComponent } from './views/exercises/create-exercise/create-exercise.component';
-// import { ShowExerciseComponent } from './views/exercises/show-exercise/show-exercise.component';
-// import { ErrorHandlerComponent } from './views/shared/error-handler/error-handler.component';
-// import { FormErrorMessagesComponent } from './views/shared/form-error-messages/form-error-messages.component';
-// import { UpdateExerciseComponent } from './views/exercises/update-exercise/update-exercise.component';
-// import { ListExerciseComponent } from './views/exercises/list-exercise/list-exercise.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-// import { FormErrorsComponent } from './components/form-errors/form-errors.component';
 import { ComponentsModule } from 'src/app/components/components.module'
 
 @NgModule({
   declarations: [
     AppComponent,
-    // HomeComponent,
-    // HomeNavbarComponent,
-    // PersonalComponent,
-    // PersonalNavbarComponent,
-    // CreateExerciseComponent,
-    // ShowExerciseComponent,
-    // ErrorHandlerComponent,
-    // FormErrorMessagesComponent,
-    // UpdateExerciseComponent,
-    // ListExerciseComponent,
     NavbarComponent,
   ],
   imports: [
