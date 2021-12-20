577c7ae7bd63461f923cda0d947af0114b4918de
joaofelipe <joaofelipe1294@gmail.com>
2021-12-20 17:36:18 -0300

    feat: add student list route.

--- src/app/modules/student/student-routing.module.ts
+++ src/app/modules/student/student-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ListStudentComponent} from './list-student/list-student.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ListStudentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
