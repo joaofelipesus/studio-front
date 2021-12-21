1bc3aa15ea84751a954ed554f05249801b9494d2
joaofelipe <joaofelipe1294@gmail.com>
2021-12-21 13:55:51 -0300

    feat: add workout plan module.

---
+++ src/app/modules/workout-plan/workout-plan.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { WorkoutPlanRoutingModule } from './workout-plan-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    WorkoutPlanRoutingModule
+  ]
+})
+export class WorkoutPlanModule { }
