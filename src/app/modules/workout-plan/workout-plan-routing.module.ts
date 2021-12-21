04190f86b89a8ccdff8af1cd4bfdec031c55bca4
joaofelipe <joaofelipe1294@gmail.com>
2021-12-21 14:44:02 -0300

    feat: add show exercise.

--- src/app/modules/workout-plan/workout-plan-routing.module.ts
+++ src/app/modules/workout-plan/workout-plan-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListWorkoutPlanComponent } from './list-workout-plan/list-workout-plan.component';
+import { ShowWorkoutPlanComponent } from './show-workout-plan/show-workout-plan.component';
 
 const routes: Routes = [
-  {path: '', component: ListWorkoutPlanComponent}
+  {path: '', component: ListWorkoutPlanComponent},
+  {path: ':id', component: ShowWorkoutPlanComponent}
 ];
 
 @NgModule({
--- src/app/modules/workout-plan/workout-plan.module.ts
+++ src/app/modules/workout-plan/workout-plan.module.ts
@@ -3,13 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { WorkoutPlanRoutingModule } from './workout-plan-routing.module';
 import { ListWorkoutPlanComponent } from './list-workout-plan/list-workout-plan.component';
+import { ShowWorkoutPlanComponent } from './show-workout-plan/show-workout-plan.component';
+import { FormsModule } from '@angular/forms';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 
 @NgModule({
-  declarations: [ListWorkoutPlanComponent],
+  declarations: [ListWorkoutPlanComponent, ShowWorkoutPlanComponent],
   imports: [
     CommonModule,
-    WorkoutPlanRoutingModule
+    WorkoutPlanRoutingModule,
+    FormsModule,
+    ComponentsModule
   ]
 })
 export class WorkoutPlanModule { }
